f9f4a8c3c63087b6748c47ea277d51e2b124f49c
Jess Latimer <jplatimer@gmail.com>
2013-11-23 00:23:37 -0800

    Initial Commit

---
+++ RoombaController/RoombaController.h
@@ -0,0 +1,13 @@
+//
+//  RoombaController.h
+//  RoombaController
+//
+//  Created by Jess Latimer on 11/23/2013.
+//  Copyright (c) 2013 Robot Friendship Society. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RoombaController : NSObject
+
+@end
---
+++ RoombaController/RoombaController.m
@@ -0,0 +1,13 @@
+//
+//  RoombaController.m
+//  RoombaController
+//
+//  Created by Jess Latimer on 11/23/2013.
+//  Copyright (c) 2013 Robot Friendship Society. All rights reserved.
+//
+
+#import "RoombaController.h"
+
+@implementation RoombaController
+
+@end
